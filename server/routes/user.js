92a626612481f03bfb6e2c61acaeef3e7f85169a
Junwon <junwon3329@gmail.com>
2019-12-13 00:58:51 +0000

    Saving in sessions of userId, not user itself

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,10 +2,20 @@ const express = require('express');
 const user = express.Router();
 const { User } = require('../models');
 
-user.post('/', async (req, res, next) => {
+user.post('/signup', async (req, res, next) => {
     try {
         const data = await new User(req.body).save();
         res.send(data);
+    } catch (error) {
+        next(error.message);
+    }
+});
+
+user.post('/authorize', async (req, res, next) => {
+    try {
+        const id = await User.findOne(req.body, '_id');
+        req.session.userId = id;
+        res.send(id);
     } catch (error) {
         next(error.message);
     }
@@ -18,6 +28,13 @@ user.get('/:id', async (req, res, next) => {
     } catch (error) {
         next('No user found');
     }
+});
+
+user.get('/signout', async (req, res, next) => {
+    req.session.destroy(err => {
+        if (err) next(err);
+        res.send('Successfully deleted');
+    });
 });
 
 module.exports = user;
