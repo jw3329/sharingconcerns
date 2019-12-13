c660a58ed944c20fd41cc04b92e3c48b21f236de
Junwon <junwon3329@gmail.com>
2019-12-13 00:57:57 +0000

    Added parameters in db itself

--- server/db/index.js
+++ server/db/index.js
@@ -2,6 +2,6 @@ const Mongoose = require('mongoose');
 
 const mongoUrl = 'mongodb://root:root123@0.0.0.0:27017/sharingconcerns?authSource=admin';
 
-Mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+Mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
 
 module.exports = Mongoose;
