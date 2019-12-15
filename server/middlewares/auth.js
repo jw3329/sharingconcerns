f7c8b20578bbaf75642018f0355d347b798f53c7
Junwon Jung <junwon3329@gmail.com>
2019-12-14 18:29:00 -0600

    Changed method to send response, as json data

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
     const { user } = req.session;
     if (!user) {
-        return res.status(401).send('No user found');
+        return res.status(401).json({ message: 'User is not authorized' });
     }
     next();
 };
