8d57fdd7012769e3aa4a9467c807e37b479a4b9f
Junwon Jung <junwon3329@gmail.com>
2019-12-22 01:21:15 +0000

    Added react router for tracking the website route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from './components/navbar';
+import { Container } from 'react-bootstrap';
+import { Signin, Signup } from './components/sign';
 
+import Home from './components/home';
 function App() {
   return (
     <div className="App">
-      Frontend
+      <BrowserRouter>
+        <Navbar />
+        <Container className="mt-3">
+          <Switch>
+            <Route exact path="/"><Home /></Route>
+            <Route exact path="/signup"><Signup /></Route>
+            <Route exact path="/signin"><Signin /></Route>
+          </Switch>
+        </Container>
+      </BrowserRouter>
     </div>
   );
 }
