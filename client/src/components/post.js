b41a12eb6ab4e5d0ae5d41f0706e1c2a76cf6073
Junwon Jung <junwon3329@gmail.com>
2019-12-24 23:02:29 +0000

    Post page created

---
+++ client/src/components/post.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+const Post = () => {
+    return (
+        <Fragment>
+            <Row>
+                <Col>
+                    My Post
+                </Col>
+            </Row>
+        </Fragment>
+    );
+}
+
+export default Post;
