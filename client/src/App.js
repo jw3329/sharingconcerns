db6fc7b1848a4f2f1629dd33c89d8fdb7393611d
Junwon Jung <junwon3329@gmail.com>
2019-12-24 21:20:41 +0000

    Create authorization method whenever refresh happens

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/navbar';
 import { Container } from 'react-bootstrap';
 import { Signin, Signup } from './components/sign';
 import Home from './components/home';
+import axios from 'axios';
+import AuthContext from './contexts/auth';
+
 function App() {
-  return (
+
+  const { setAuth } = useContext(AuthContext);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    axios.get('/user')
+      .then(res => res.data)
+      .then(({ status, user }) => status && setAuth(user))
+      .catch(err => console.log(err))
+      .finally(() => setLoaded(true));
+  }, [loaded, setAuth]);
+
+  return loaded && (
     <div className="App">
       <BrowserRouter>
         <Navbar />
