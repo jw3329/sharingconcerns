09798fb7569f7ed71a9644c0476d52a85db48ab7
Junwon Jung <junwon3329@gmail.com>
2019-12-24 21:21:31 +0000

    Installed react-cookie package, which will set up cookies in react project

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import axios from 'axios';
 import { AuthProvider } from './contexts/auth';
+import { CookiesProvider } from 'react-cookie';
 
 axios.defaults.baseURL = "http://localhost:8000/api";
-ReactDOM.render(<AuthProvider ><App /></AuthProvider>, document.getElementById('root'));
+axios.defaults.withCredentials = true;
+
+ReactDOM.render(<CookiesProvider><AuthProvider ><App /></AuthProvider></CookiesProvider>, document.getElementById('root'));
