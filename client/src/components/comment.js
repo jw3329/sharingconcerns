be1ab1a18076e24fd55788e034f0cea23fad93d2
Junwon Jung <junwon3329@gmail.com>
2020-01-04 22:43:30 +0000

    Big structure created for post comment and replying the comment

--- client/src/components/comment.js
+++ client/src/components/comment.js
@@ -12,6 +12,7 @@ const Comment = ({ id }) => {
     const { auth } = useContext(AuthContext);
     const [idMapReply, setIdMapReply] = useState({});
     const [idMapReplyMessage, setIdMapReplyMessage] = useState({});
+    const [idMapShowReply, setIdMapShowReply] = useState({});
 
     useEffect(() => {
         axios.get(`/post/${id}/comments`)
@@ -44,8 +45,14 @@ const Comment = ({ id }) => {
         }
     }
 
-    const handleShowReply = () => {
-
+    const handleShowReply = async comment => {
+        try {
+            const { status, replies, message } = (await axios.get(`/comment/${comment._id}/replies`)).data;
+            if (!status) throw new Error(message);
+            setIdMapShowReply({ ...idMapShowReply, [comment._id]: replies });
+        } catch (error) {
+            console.log(error.message);
+        }
     }
 
     const handleReplyChange = (e, comment) => {
@@ -106,12 +113,42 @@ const Comment = ({ id }) => {
                     </div>
                     <div>
                     </div>
-                    <div>
-                        <button className="btn btn-link m-2" onClick={() => handleShowReply(comment)}>Show Replies({comment.replies.length})</button>
+                    <div className="m-3">
+                        <Accordion.Toggle as={Button} variant="link" onClick={() => handleShowReply(comment)} eventKey="1">Show Replies({comment.replies.length})</Accordion.Toggle>
+                        {idMapShowReply[comment._id] && (
+                            <Accordion.Collapse eventKey="1">
+                                <Fragment>
+                                    {idMapShowReply[comment._id].map((reply, key) => makeReplyCard(reply, key))}
+                                </Fragment>
+                            </Accordion.Collapse>
+                        )}
                     </div>
                 </div>
             </div>
         </Accordion>
+    );
+
+    const makeReplyCard = (reply, key) => (
+        <div className="card m-3" key={key}>
+            <div className="card-body">
+                <div className="row">
+                    <div className="col-sm-9">
+                        {reply.description}
+                    </div>
+                    <div className="ml-auto">
+                        <div className="m-2">
+                            {Utils.toLocaleTimestamp(reply.updateDate)}
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="ml-auto">
+                        <button className={`btn btn${reply.likes.includes(auth._id) ? '' : '-outline'}-success m-2`} onClick={() => handleLike(reply)}>Like({reply.likes.length})</button>
+                        <button className={`btn btn${reply.dislikes.includes(auth._id) ? '' : '-outline'}-danger m-2`} onClick={() => handleDislike(reply)}>Dislike({reply.dislikes.length})</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 
     const handleSubmit = async e => {
