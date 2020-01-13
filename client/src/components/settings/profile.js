0c8b73f689d94af72e76da3c91bc6afe664440f3
Junwon <junwon3329@gmail.com>
2020-01-13 22:42:35 +0000

    Basic form for settings profile tab has been completed

--- client/src/components/settings/profile.js
+++ client/src/components/settings/profile.js
@@ -1,7 +1,57 @@
 import React from 'react';
+import { Form, Row, Col, Button } from 'react-bootstrap';
 
 const Profile = () => {
-    return (<div>profile page</div>);
+
+    const handleSubmit = e => {
+
+    }
+
+    const handleChange = e => {
+
+    }
+
+
+    return (
+        <Form onSubmit={handleSubmit} onChange={handleChange}>
+            <Row className="justify-content-md-center">
+                <Col sm={6}>
+                    <Form.Group controlId="firstName">
+                        <Form.Label>Firstname</Form.Label>
+                        <Form.Control type="text" placeholder="Firstname" />
+                    </Form.Group>
+                    <Form.Group controlId="lastName">
+                        <Form.Label>Lastname</Form.Label>
+                        <Form.Control type="text" placeholder="Lastname" />
+                    </Form.Group>
+                    <Form.Group controlId="bio">
+                        <Form.Label>Bio</Form.Label>
+                        <Form.Control type="text" placeholder="Bio" />
+                    </Form.Group>
+                    <Form.Group controlId="url">
+                        <Form.Label>URL</Form.Label>
+                        <Form.Control type="text" placeholder="url" />
+                    </Form.Group>
+                    <Form.Group controlId="company">
+                        <Form.Label>Company</Form.Label>
+                        <Form.Control type="text" placeholder="company" />
+                    </Form.Group>
+                    <Form.Group controlId="location">
+                        <Form.Label>Location</Form.Label>
+                        <Form.Control type="text" placeholder="location" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">Update</Button>
+                    {/* {
+                        validation.status !== null && (
+                            <Alert className="mt-3" variant={validation.status ? 'success' : 'danger'}>
+                                {validation.message}
+                            </Alert>
+                        )
+                    } */}
+                </Col>
+            </Row>
+        </Form>
+    );
 }
 
 export default Profile;
