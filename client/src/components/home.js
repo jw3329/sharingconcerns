3fd860fb366c2677ba7d3f2e9f0b1e058a2c43e2
Junwon <junwon3329@gmail.com>
2020-01-21 17:44:46 +0000

    Home page is now showing differently to authorized, and unauthorized, authorized user is now seeing recent activities from followees and my posts

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import AuthContext from '../contexts/auth';
+import Axios from 'axios';
+import Utils from '../utils';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-    return (
-        <div>This is home</div>
-    );
+
+    const { auth } = useContext(AuthContext);
+    const [activities, setActivities] = useState([]);
+
+    document.title = 'Home';
+
+    console.log(activities)
+
+    useEffect(() => {
+        if (auth) {
+            Axios.get('/user/activities')
+                .then(res => res.data)
+                .then(({ status, activities, message }) => {
+                    if (!status) throw new Error(message);
+                    setActivities(activities);
+                })
+                .catch(error => console.log(error.message));
+        }
+    }, [auth]);
+
+    const generateCard = (activity, key) => (
+        <div className="card m-3" key={key}>
+            <div className="card-body">
+                <div className="row">
+                    <div className="col-sm-3">
+                        {Utils.getImageElement(activity.user._id, activity.user.profileImage)}
+                    </div>
+                    <div className="col-sm-6">
+                        <Link to={`/post/${activity._id}`}>{activity.title}</Link>
+                    </div>
+                    <div className="col-sm-3" style={{ fontSize: "smaller" }}>
+                        <div>{`${activity.views} views`}</div>
+                        <div>{activity.user.username}</div>
+                        <div>{Utils.toLocaleTimestamp(activity.updateDate)}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+    return auth ? (
+        <div className="row">
+            <div className="col-sm-6">
+                <h3 className="d-flex justify-content-center">Recent activities</h3>
+                {activities.map((activity, key) => generateCard(activity, key))}
+            </div>
+        </div>
+    ) : (
+            <div>Please sign in to start</div>
+        );
 }
 
 export default Home;
