ab55856e5923d1438545474c516f36db4d948461
Junwon Jung <junwon3329@gmail.com>
2020-01-21 07:19:10 +0000

    Followers and followees list have been added to userpage

--- client/src/components/userpage.js
+++ client/src/components/userpage.js
@@ -3,6 +3,7 @@ import Axios from 'axios';
 import Error from '../components/error';
 import AuthContext from '../contexts/auth';
 import PostCards from './postcards';
+import Follow from './follow';
 
 const UserPage = props => {
 
@@ -42,15 +43,28 @@ const UserPage = props => {
     return loaded && (
         user ? (
             <Fragment>
-                <h1>{username}'s page</h1>
-                {auth._id !== user._id && (
-                    <div className="d-flex justify-content-end">
-                        <button onClick={handleFollow} className={`btn btn-${user.followers.includes(auth._id) ? 'secondary' : 'primary'} m-2`}>{user.followers.includes(auth._id) ? 'Unfollow' : 'Follow'}</button>
+                <div className="d-flex">
+                    <h1>{username}'s page</h1>
+                    {auth._id !== user._id && (
+                        <div className="ml-auto">
+                            <button onClick={handleFollow} className={`btn btn-${user.followers.find(follower => follower._id === auth._id) ? 'secondary' : 'primary'} m-2`}>{user.followers.find(follower => follower._id === auth._id) ? 'Unfollow' : 'Follow'}</button>
+                        </div>
+                    )}
+                </div>
+                <div className="row p-3">
+                    <div className="col-sm-6 border-right">
+                        <h3>Post</h3>
+                        <PostCards posts={posts} />
                     </div>
-                )}
-                <h3>Post</h3>
-                <PostCards posts={posts} />
-
+                    <div className="col-sm-3 border-right text-center">
+                        <h3>Followers</h3>
+                        <Follow follows={user.followers} />
+                    </div>
+                    <div className="col-sm-3 text-center">
+                        <h3>Followees</h3>
+                        <Follow follows={user.followees} />
+                    </div>
+                </div>
             </Fragment>
         ) : (
                 <Error />
