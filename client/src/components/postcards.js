9fa50f243f0575c8c597a33508addbf78f9665d6
Junwon Jung <junwon3329@gmail.com>
2020-01-21 07:19:35 +0000

    Edited size of image, w-100 h-100 to w-100 only

--- client/src/components/postcards.js
+++ client/src/components/postcards.js
@@ -6,16 +6,16 @@ import { Link } from 'react-router-dom';
 const PostCards = ({ posts }) => {
 
     const generatePostCard = (post, key) => (
-        <Card className="my-3" style={{ width: '30rem' }} key={key}>
+        <Card className="my-3" key={key}>
             <Card.Body>
                 <div className="row">
-                    <div className="col-sm-3">
-                        <img className="w-100 h-100 rounded-circle" src={Utils.getProfileImageLink(post.user._id, post.user.profileImage)} alt="" />
+                    <div className="col-sm-4">
+                        <img className="w-100 rounded-circle" src={Utils.getProfileImageLink(post.user._id, post.user.profileImage)} alt="" />
                     </div>
-                    <div className="col-sm-6">
+                    <div className="col-sm-4">
                         <Card.Title> <Link to={`/post/${post._id}`}>{post.title}</Link></Card.Title>
                     </div>
-                    <div className="col-sm-3" style={{ fontSize: "smaller" }}>
+                    <div className="col-sm-4" style={{ fontSize: "smaller" }}>
                         <div>{`${post.views} views`}</div>
                         <div>{post.user.username}</div>
                         <div>{Utils.toLocaleTimestamp(post.updateDate)}</div>
