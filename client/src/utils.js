4c3d58c2d099bde7975939309dc3a1c51d3c725b
Junwon <junwon3329@gmail.com>
2020-01-22 20:18:32 +0000

    Utils has been updated, timestamp has been fixed to relative to current time

--- client/src/utils.js
+++ client/src/utils.js
@@ -8,8 +8,29 @@ class Utils {
     static server = `http://${this.host}:${this.port}`;
 
     static toLocaleTimestamp(timestamp) {
-        const date = new Date(timestamp);
-        return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
+        const now = new Date();
+        const past = new Date(timestamp);
+        const elapsed = now.getTime() - past.getTime();
+
+        const msPerMinute = 60 * 1000;
+        const msPerHour = msPerMinute * 60;
+        const msPerDay = msPerHour * 24;
+        const msPerMonth = msPerDay * 30;
+        const msPerYear = msPerDay * 365;
+
+        if (elapsed < msPerMinute) {
+            return Math.round(elapsed / 1000) + ' seconds ago';
+        } else if (elapsed < msPerHour) {
+            return Math.round(elapsed / msPerMinute) + ' minutes ago';
+        } else if (elapsed < msPerDay) {
+            return Math.round(elapsed / msPerHour) + ' hours ago';
+        } else if (elapsed < msPerMonth) {
+            return Math.round(elapsed / msPerDay) + ' days ago';
+        } else if (elapsed < msPerYear) {
+            return Math.round(elapsed / msPerMonth) + ' months ago';
+        } else {
+            return Math.round(elapsed / msPerYear) + ' years ago';
+        }
     }
 
     static getProfileImageLink(userId, profileImage) {
