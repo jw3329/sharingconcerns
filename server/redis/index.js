f8bd1f8cef600b15f3dd18329af2fe1b4f2bfb92
Junwon Jung <junwon3329@gmail.com>
2020-02-25 01:37:26 -0600

    Backend redis server configuration completed

--- server/redis/index.js
+++ server/redis/index.js
@@ -1,11 +1,16 @@
 const redis = require('redis');
 const { promisify } = require('util');
-const REDIS_URL = "redis://cache";
+const REDIS_URL = "redis://0.0.0.0";
 const client = redis.createClient(REDIS_URL);
+
+client.on('connect', () => {
+    console.log('Redis connected');
+});
 
 module.exports = {
     ...client,
     getAsync: promisify(client.get).bind(client),
     setAsync: promisify(client.set).bind(client),
+    delAsync: promisify(client.del).bind(client),
     keysAsync: promisify(client.keys).bind(client)
 };
